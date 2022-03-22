9f1aac6601b86a8a9519e9b355b806e2de51c4e9
Bendtner11 <bendtner.dave@gmail.com>
2022-03-22 16:43:31 +0000

    field fixing in salary slip and employee

--- erptn/overrides/salary_slip/salary_slip.py
+++ erptn/overrides/salary_slip/salary_slip.py
@@ -1,18 +1,18 @@
-from erpnext.payroll.doctype.salary_slip.salary_slip import SalarySlip
+#from erpnext.payroll.doctype.salary_slip.salary_slip import SalarySlip
 
-import frappe
-from frappe.utils import flt
-from erpnext.utilities.transaction_base import TransactionBase
-from frappe.model.document import Document
-class CustomSalarySlip(SalarySlip):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-
-	def validate(self):
-		super(CustomSalarySlip, self).validate()
-		self.salary_imp = self.salary_imp - self.gross_pay
-	def my_custom_code(self):
-		pass
+#import frappe
+#from frappe.utils import flt
+#from erpnext.utilities.transaction_base import TransactionBase
+#from frappe.model.document import Document
+#class CustomSalarySlip(SalarySlip):
+#	def __init__(self, *args, **kwargs):
+#		super().__init__(*args, **kwargs)
+#
+#	def validate(self):
+#		super(CustomSalarySlip, self).validate()
+#		self.salary_imp = self.salary_imp - self.gross_pay
+#	def my_custom_code(self):
+#		pass
 #		self.salary_imp =self.gross_pay -
 		#for d in self._salary_structure_doc.get("deductions"):
 		#	if d.salary_component_abbr == "CS":
