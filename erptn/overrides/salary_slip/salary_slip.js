b795c3fc46c0125a7f07a1a2d821c709cda1524a
Bendtner11 <bendtner.dave@gmail.com>
2022-03-23 16:24:40 +0000

    calcul Salaire net imposable

--- erptn/overrides/salary_slip/salary_slip.js
+++ erptn/overrides/salary_slip/salary_slip.js
@@ -1,12 +1,46 @@
-//frappe.ui.form.on('Salary Slip', {
-//	employee:function(frm) {
+frappe.ui.form.on('Salary Slip', {
+	employee:function(frm) {
+		var parent = 0 ;
+		var cot_pro = 0 ;
+		var deduct=0;
+		var npc=0;
+//		frm.doc.tax_salary = 0 ;
+//		frm.doc.tot_deduct = 0 ;
+		frappe.call({
+                method: "frappe.client.get",
+                args: {
+                    doctype: "Employee",
+                    name: frm.doc.employee,
+                },
+                callback(r) {
+                    if(r.message) {
+                        var empl = r.message;
+                        frm.set_value("deduct_impot",empl.deduct_impot || 0);
+                        frm.set_value("n_p_c",empl.n_p_c || 0);
+			deduct=empl.deduct_impot;
+			npc=empl.n_p_c || 0;
+                    }}
+               });
+	       frm.refresh_field("deduct_impot");
+	       frm.refresh_field("n_p_c");
 	           // console.log(frm.doc.deductions);
-	//for(var i=0;i<cur_frm.doc.deductions.length;i++){
-	  //  if (frm.doc.deductions[i].abbr == "CS" || frm.doc.deductions[i].abbr == "cnss"){
-//
-//	         frm.doc.social_salary = (frm.doc.gross_pay - parseFloat(frm.doc.deductions[i].amount))*12;
-//	    }
-//	    }
-//    }
-//    refresh_field("social_salary");
-//})
+	for(var i=0;i<cur_frm.doc.deductions.length;i++){
+	    if (frm.doc.deductions[i].abbr == "CS" || frm.doc.deductions[i].abbr == "cnss"){
+
+	         frm.doc.social_salary = (frm.doc.gross_pay - parseFloat(frm.doc.deductions[i].amount))*12;
+	    }
+	    }
+	cot_pro = frm.doc.social_salary * 0.1 ;
+	if ( cot_pro > 2000 ){
+	cot_pro = 2000;
+	}
+	parent = frm.doc.social_salary * 0.05;
+	if (parent < 450) {
+	parent = npc * parent ;}
+	else {
+	parent = 450 * npc;}
+	frm.doc.tot_deduct += deduct + parent + cot_pro;
+        frm.doc.tax_salary =frm.doc.social_salary - frm.doc.tot_deduct;
+    	}
+
+ })
